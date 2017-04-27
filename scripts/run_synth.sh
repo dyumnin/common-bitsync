ac6fec220f372255238011e3666c953d769378e7
Vijayvithal <jvs@dyumnin.com>
2017-04-27 05:50:28 +0530

    Fixed weekly regression dummy script.

--- scripts/run_synth.sh
+++ scripts/run_synth.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
  /prj/tools/qflow/share/qflow/scripts/synthesize.sh /prj/dyumnin/designw.in/hdl/common-bitsync bitsync
- /prj/tools/qflow/share/qflow/scripts/placement.sh -d /prj/dyumnin/designw.in/hdl/common-bitsync bitsync
- /prj/tools/qflow/share/qflow/scripts/vesta.sh /prj/dyumnin/designw.in/hdl/common-bitsync bitsync
